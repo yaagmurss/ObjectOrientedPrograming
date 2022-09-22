f207e64f6386e5522bd75f64dd3cf75bd957bee9
Yagmur <52479605+yaagmurss@users.noreply.github.com>
2022-09-22 16:20:50 +0300

    Add files via upload
    UpCasting And DownCasting

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using ObjectOrientedProgrammingForCSharp.TopicsOfOOP.Introduction;
 using ObjectOrientedProgrammingForCSharp.TopicsOfOOP._1._1._Constructor;
 using ObjectOrientedProgrammingForCSharp.TopicsOfOOP._1._2._Fields;
 using ObjectOrientedProgrammingForCSharp.TopicsOfOOP._2._Relationships_Between_Classes.Inheritence;
-using ClassLibraryForOOP.AccesModifiers;
+using ObjectOrientedProgrammingForCSharp.TopicsOfOOP._2._Relationships_Between_Classes._2._2._UpCastingDownCasting;
 
 namespace ObjectOrientedProgrammingForCSharp
 {
@@ -21,20 +21,24 @@ namespace ObjectOrientedProgrammingForCSharp
 
         static void Main(string[] args)
         {
+            Console.WriteLine("-----------Introduction---------");
             //Introduction
             var person = new Person();
             person.Name = "Yagmur";
             person.Introduce();
 
+            Console.WriteLine("-----------Constructor---------");
             //Constructor
             var emp = new Employee("Yagmur");
             emp.EmployeeOfMonth();
 
+            Console.WriteLine("-----------Fields---------");
             //Fields
             var cs = new Customer();
             var list = new List<string> { "One", "Two", "Three" };
             cs.Orders = list;
 
+            Console.WriteLine("-----------Methods---------");
             //Methods
             var point = new Point();
             Console.WriteLine("PhoneNumber is : " + point.Caller());
@@ -42,22 +46,33 @@ namespace ObjectOrientedProgrammingForCSharp
             point.Add( 1, 2, 3, 4 ); //If you use params yo can add values like this instead of below
             point.Add(new int[] { 1, 2, 3, 4 });
 
+            Console.WriteLine("-----------Modifiers---------");
             //Acces Modifiers
             var john = new Customers();
-            var promote = new CustomerPromoter();
-            promote.CalculatePromotionCase1(1);         
+            var customer = new Customers();
+            customer.GetPrometed(5);
 
-            
-
+            Console.WriteLine("-----------Inheritence---------");
             //Inheritence
-            var chopping = new Chopping();
-            chopping.HeatingFood();
+            var chopping = new Chopping(5);
 
+            Console.WriteLine("-----------Composition---------");
             //Composition
             //???
-            
 
-            
+
+            Console.WriteLine("-----------UpCasting---------");
+            //UpCasting
+
+            Text text = new Text();
+            Shape shape = text;
+
+            text.Width = 100;
+            shape.Width = 200;
+
+            Console.WriteLine("Object Width is : {0} ", text.Width); //Returns 200
+
+
 
 
         }
